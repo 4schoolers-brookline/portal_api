35bde93dbd557f92ca43fd921b2dce4f65390820
Daniyar <daniyardake@gmail.com>
2021-06-02 10:15:14 -0400

    first commit

---
+++ lesson/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+#class Lesson:
+    # student(s)
+    # teacher
+    # homework -> submission
+    # classwork  -> submission
+    # datetime
+    # duration
+
+# class Submission():
+    # owner
+    # name
+    # description
+    # file
+    # type
+
