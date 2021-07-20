6d7227b9331bc5b40a7852aa2ee26819e5362282
Daniyar <daniyardake@gmail.com>
2021-07-20 15:38:30 -0400

    scripts footprint added

---
+++ scripts/add_students.py
@@ -0,0 +1,17 @@
+import os, django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal.settings")
+django.setup()
+
+from django.contrib.auth.models import User
+from activity.models import Lesson
+from employee.models import Employee
+from student.models import Student
+
+
+def main():
+    file_path = input('csv file location: ')
+
+    return 0
+
+if __name__ == '__main__':
+    main()
