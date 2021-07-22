65114f03ac3009cc0310fee0b0038ad40d9bd3b9
Nabi Kussayin <nabikussain@gmail.com>
2021-07-22 21:20:20 +0600

    Scripts for employees and students

--- scripts/add_employees.py
+++ scripts/add_employees.py
@@ -1,6 +1,10 @@
+import sys
+sys_path = input("System path to project")
+sys.path.append(sys_path)
 import os, django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal.settings")
 django.setup()
+import csv
 
 from django.contrib.auth.models import User
 from activity.models import Lesson
@@ -8,9 +12,55 @@ from employee.models import Employee
 from student.models import Student
 
 def main():
-    file_path = input('csv file location: ')
-
-    # NOTE: Make saving safe: if employee with the same username exists dont create another one
+    path = input("File path: ")
+    with open(path) as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            user, created = User.objects.get_or_create(
+                first_name=row[0],
+                last_name=row[1],
+                username=row[2],
+                email=row[3],
+                password=row[4]
+                )
+            if created:
+                employee, created = Employee.objects.get_or_create(
+                    user = user,
+                    title = row[6],
+                    education = row[7],
+                    languages = row[9],
+                    interests = row[10],
+                    subjects = row[11],
+                    phone = row[14],
+                    bio = row[15],
+                    school = row[16],
+                    address = row[18],
+                    city = row[19],
+                    zip_code = row[20],
+                    state = row[21],
+                    country = row[22],
+                    level = row[23]
+                )
+                if created:
+                    try:
+                        employee.birth_date=row[12]
+                        employee.save()
+                    except:
+                        employee.birth_date= None
+                    try:
+                        employee.graduation_year=row[17]
+                        employee.save()
+                    except:
+                        employee.graduation_year= None
+                    try:
+                        employee.priority = row[8]
+                        employee.save()
+                    except:
+                        employee.priority = None
+                    employee.is_male = (row[12] == "yes" )
+                    employee.is_fulltime = (row[5] == "yes")
+                    employee.save()
     return 0
 
 if __name__ == '__main__':
--- scripts/add_students.py
+++ scripts/add_students.py
@@ -1,16 +1,51 @@
+import sys
+sys_path = input("System path to project")
+sys.path.append(sys_path)
 import os, django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal.settings")
 django.setup()
+import csv
 
 from django.contrib.auth.models import User
 from activity.models import Lesson
 from employee.models import Employee
 from student.models import Student
 
-
 def main():
-    file_path = input('csv file location: ')
-    # NOTE: Make saving safe: if employee with the same username exists dont create another one
+    path = input("File path: ")
+    with open(path) as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            user, created = User.objects.get_or_create(
+                first_name=row[0],
+                last_name=row[1],
+                username=row[2],
+                email=row[4],
+                password=row[3]
+                )
+            if created:
+                student, created = Student.objects.get_or_create(
+                    user = user,
+                    phone = row[5],
+                    bio = row[6],
+                    languages = row[8],
+                    interests = row[9],
+                    school = row[10],
+                    address = row[12],
+                    city = row[13],
+                    zip_code = row[14],
+                    state = row[15],
+                    country = row[16],
+                )
+                if created:
+                    try:
+                        student.graduation_year=row[11]
+                        student.save()
+                    except:
+                        student.graduation_year= None
+                    student.is_male = (row[7] == "yes" )
+                    student.save()
     return 0
 
 if __name__ == '__main__':
