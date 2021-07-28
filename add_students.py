07c3a6ee115ef1a9ee10ed511b07a6b54f8a3d15
Daniyar <daniyardake@gmail.com>
2021-07-28 11:36:58 -0400

    script modified

--- add_students.py
+++ add_students.py
@@ -11,7 +11,58 @@ from activity.models import Lesson
 from employee.models import Employee
 from student.models import Student
 
-def main():
+def add_student():
+    data = input('First Name,Last Name,username,password,Email,Phone,bio,is_male,languages,interests,school,graduation year,address,city,zip,state,country').split(',')
+    
+    # Data
+    fname = data[0]
+    lname = data[1]
+    username = data[2]
+    password = data[3]
+    email = data[4]
+    phone = data[5]
+    bio = data[6]
+    is_male = (data[7]=='yes')
+    languages = data[8]
+    interests = data[9]
+    school = data[10]
+    graduation = data[11]
+    address = data[12]
+    city = data[13]
+    zip_code = data[14]
+    state = data[15]
+    country = data[16]
+
+    #user creation
+    user = User(
+        first_name = fname,
+        last_name = lname,
+        username = username,
+        email = email
+    )
+    user.set_password(password)
+    user.save()
+    
+    # student creation
+    student = Student(
+        user = user,
+        phone = phone,
+        bio = bio,
+        is_male = is_male,
+        languages = languages,
+        interests = interests,
+        school = school,
+        address = address,
+        city = city,
+        zip_code = zip_code,
+        state = state,
+        country = country,
+        is_active = True
+    )
+    student.save()
+    return 0
+
+def read_file():
     path = input("File path: ")
     with open(path) as f:
         reader = csv.reader(f)
@@ -48,6 +99,15 @@ def main():
                     student.is_male = (row[7] == "yes" )
                     student.save()
     return 0
+
+
+def main():
+    x = input('File(1) or CLI(2)')
+    if (x=='1'):
+        read_file()
+    elif (x=='2'):
+        add_student()
+    
 
 if __name__ == '__main__':
     main()
