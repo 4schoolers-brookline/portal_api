6b15d816c220748c67b23777f6fc1c53488b8527
Daniyar <daniyardake@gmail.com>
2021-08-30 09:50:05 -0400

    models added

--- corporation/models.py
+++ corporation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from student.models import Student
+from employee.models import Employee
 
-# Create your models here.
+class Manager(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    children = models.ManyToManyField(Employee)
+
+    def __str__(self):
+        return self.user.get_full_name()
